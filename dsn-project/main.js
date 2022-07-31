0e1e22b598c927b01cc62a7036d063b718e5c9f9
Kunal <89037807+Kk015@users.noreply.github.com>
2022-07-31 15:51:18 +0530

    commit 3

--- dsn-project/main.js
+++ dsn-project/main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
-import { ACESFilmicToneMapping } from "three";
+import { BoxGeometry, CylinderBufferGeometry, Vector2 } from "three";
+import * as STBLIB from "three-stdlib";
+import { mergeBufferAttributes, mergeBufferGeometries } from "three-stdlib";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("#FFEECC");
@@ -19,15 +21,45 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.physicallyCorrectLights = true;
 document.body.appendChild(renderer.domElement);
 
-let sphereMesh = new THREE.Mesh(
-  new THREE.SphereGeometry(5, 10, 10),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
+const controls = new STBLIB.OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+controls.damagingFactor = 0.05;
+controls.enableDamping = true;
 
-scene.add(sphereMesh);
+let envmap;
 
 (async function () {
+  let pmrem = new THREE.PMREMGenerator(renderer);
+  let envmapTexture = await new STBLIB.RGBELoader()
+    .setDataType(THREE.FloatType)
+    .loadAsync("/envmap.hdr");
+  envmap = pmrem.fromEquirectangular(envmapTexture).texture;
+
+  makeHex(3, new THREE.Vector2(0, 0));
+  let hexagonMesh = new THREE.Mesh(
+    hexagonGeometries,
+    new THREE.MeshStandardMaterial({
+      envMap: envmap,
+      flatShading: true,
+    })
+  );
+  scene.add(hexagonMesh);
+
   renderer.setAnimationLoop(() => {
+    controls.update();
     renderer.render(scene, camera);
   });
 })();
+
+let hexagonGeometries = new THREE.BoxGeometry(0, 0, 0);
+
+function hexGeometry(height, position) {
+  let geo = new THREE.CylinderGeometry(1, 1, height, 6, 1, false);
+  geo.translate(position.x, height * 0.5, position.y);
+  return geo;
+}
+
+function makeHex(height, position) {
+  let geo = hexGeometry(height, position);
+  hexagonGeometries = mergeBufferGeometries([hexagonGeometries, geo]);
+}
